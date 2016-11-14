7395aa601d75778648f1503bd42a40f8a7f34085
whoansoo <whoansoo@gmail.com>
2016-11-14 23:02:43 +0900

    요구사항 1 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,8 +1,12 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -21,16 +25,91 @@ public class RequestHandler extends Thread {
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+        
+        String ReponsString = "";
+        
+        try (InputStream   in = connection.getInputStream(); 
+             OutputStream out = connection.getOutputStream()) {
+        	
+        	String uriFile = parseURL(in);
+        	if (uriFile.equals("error")) {
+        		log.error("null error");
+        		ReponsString = "error";
+        	} else {
+        		uriFile = "./" + uriFile;
+        		ReponsString = ReadIndexFile(uriFile);
+        	}
+        	
+        	sendResponse(ReponsString, out);
+                    
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    
+    private void  sendResponse(String ReponsString, OutputStream out) {
+    	
+    	DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = ReponsString.getBytes();
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+    
+    private String parseURL(InputStream in) {
+    	String line = "";
+    	String tmpLine[];
+    	
+		try {
+			    			
+	        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	        
+	        StringBuffer request = new StringBuffer();
+	        while(true) {
+	        	int ch = br.read();
+	        	if ((ch < 0) || (ch == '\n')) {
+                    break;
+                }
+                request.append((char) ch);
+	    	} 
+	    		
+	    	line = request.toString();
+	    	System.out.println(line);
+	    	
+	    	tmpLine = line.split(" ");
+    	
+	    	if (!tmpLine[1].equals("/index.html") || tmpLine[1] == "" )
+	    		return "error";
+	    	else 
+	    		return tmpLine[1];
+	    	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "error";	
+		    
+    }
+    
+    private String ReadIndexFile(String uriFile) {
+
+        int i;
+        String line = "";
+		try {
+			InputStream is;
+			is = new FileInputStream(uriFile);
+	        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+	        
+	        StringBuffer request = new StringBuffer();
+	    	while((line = br.readLine()) != null) {
+	    		request.append(line);	
+	    		request.append("\r");
+	    	}
+	    	line = request.toString();
+	    	
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}    	
+    	return line;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
